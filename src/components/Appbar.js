2f65faa6e98c326994c54ced2619f8a628344f47
Ayush Khamrui <Ayushkhamrui@outlook.com>
2022-08-01 22:19:08 +0530

    second

---
+++ src/components/Appbar.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+export default function Appbar() {
+  return (
+    <div className='Appbar'>
+        <div class="container-fluid">
+            <header class="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
+                <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-dark text-decoration-none">
+                    <span class="fs-4"><h3>JobX</h3></span>
+                </a>
+
+                <ul class="nav nav-pills">
+                    <li class="nav-item"><a href="#" class="nav-link active" aria-current="page">Home</a></li>
+                    <li class="nav-item"><a href="#" class="nav-link">Resources</a></li>
+                    <li class="nav-item"><a href="#" class="nav-link">Jobs</a></li>
+                    <li class="nav-item"><a href="#" class="nav-link">About</a></li>
+                </ul>
+            </header>
+        </div>
+    </div>
+  )
+}
---
+++ src/pages/Home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Appbar from '../components/Appbar'
+import Cover from '../components/Cover'
+
+const Home = () => {
+  return (
+    <div>
+        <Appbar/>
+        <Cover/>
+    </div>
+  )
+}
+
+export default Home
